1ba9d406585c5262c5acb7e999cbf153c74bb27c
Najman <najman.husaini@utah.edu>
2023-03-08 20:10:42 -0700

    Refactor, and preserved order of images

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,25 @@
 #![feature(iterator_try_collect)]
 
 use std::fs::read_dir;
-use std::mem::{forget, transmute};
+use std::mem::{forget, transmute, MaybeUninit};
 use std::net::{Ipv4Addr, SocketAddrV4};
 use std::ops::Deref;
+use std::sync::mpsc::channel;
 use std::time::Instant;
 
 use axum::http::HeaderValue;
 use axum::response::IntoResponse;
-use axum::routing::{get};
+use axum::routing::get;
 use axum::{Router, Server};
 use fern::Dispatch;
 use image::codecs::jpeg::JpegDecoder;
 use image::DynamicImage;
 use log::{error, LevelFilter};
+use rayon::spawn;
 use std::io::{Cursor, Read, Write};
-use std::sync::mpsc::channel;
 use tower_http::compression::CompressionLayer;
 use zip::write::FileOptions;
 use zip::ZipWriter;
-
-type SharedEntry = (
-    // image name
-    String,
-    // Preview image
-    &'static [u8],
-    // Full image
-    &'static [u8]
-);
 
 #[tokio::main]
 async fn main() {
@@ -54,87 +46,80 @@ async fn main() {
         .apply()
         .unwrap();
 
-    let listdir = read_dir(".")
+    let entries = read_dir(".")
         .expect("Reading current directory")
-        .map(|res| {
-            res.map(|path| {
-                match path.path().extension().map(|x| x.to_str()).flatten() {
-                    Some("jpg" | "jpeg") => Some(path),
-                    _ => None
-                }
-            })
-            .transpose()
+        .filter_map(|res| {
+            let entry = res.expect("Error listing entry");
+            match entry.path().extension().map(|x| x.to_str()).flatten() {
+                Some("jpg" | "jpeg") => Some(entry),
+                _ => None,
+            }
         })
-        .filter_map(|entry| entry)
-        .try_collect::<Vec<_>>()
-        .expect("Listing entry");
+        .collect::<Vec<_>>();
 
-    // Acts as a JoinHandle as rayon::spawn doesn't give one
-    let (ready_sender, ready_receiver) = channel::<SharedEntry>();
+    let entry_count = entries.len();
 
-    // Multithreaded image loading and preview generation
-    for file in listdir {
-        let path = file.path();
+    let (image_sender, image_receiver) = channel();
 
-        match path.extension().map(|x| x.to_str()).flatten() {
-            Some("jpg" | "jpeg") => {}
-            _ => continue,
-        }
+    entries
+        .into_iter()
+        .zip(vec![image_sender; entry_count])
+        .enumerate()
+        .for_each(|(i, (file, image_sender))| {
+            // Multithreaded image loading and preview generation
+            spawn(move || {
+                let path = file.path();
 
-        let ready_sender = ready_sender.clone();
+                // I read the whole image to memory, even though there is a method in image
+                // to do that. For some reason, this is around 10x faster
+                let mut file = std::fs::File::open(&path).expect("Loading image");
 
-        rayon::spawn(move || {
-            // I read the whole image to memory, even though there is a method in image
-            // to do that. For some reason, this is around 10x faster
-            let mut file = std::fs::File::open(&path).expect("Loading image");
+                let mut full = Vec::new();
+                file.read_to_end(&mut full).expect("Reading image");
 
-            let mut full = Vec::new();
-            file.read_to_end(&mut full).expect("Reading image");
+                let mut decoder =
+                    JpegDecoder::new(full.as_slice()).expect("Initializing decoder for image");
+                decoder.scale(900, 600).expect("Scaling image");
+                let image = DynamicImage::from_decoder(decoder).expect("Decoding image");
 
-            let mut decoder =
-                JpegDecoder::new(full.as_slice()).expect("Initializing decoder for image");
-            decoder.scale(900, 600).expect("Scaling image");
-            let image = DynamicImage::from_decoder(decoder).expect("Decoding image");
+                let preview = webp::Encoder::from_image(&image)
+                    .expect("Encoding to webp")
+                    .encode(35.0);
 
-            let preview = webp::Encoder::from_image(&image)
-                .expect("Encoding to webp")
-                .encode(35.0);
+                // Leak full as it will be used for the duration of the program,
+                // but will not be modified, so we don't need the extra data in Vec
+                let full: &[u8] = full.leak();
 
-            // Leak full as it will be used for the duration of the program,
-            // but will not be modified, so we don't need the extra data in Vec
-            let full: &[u8] = full.leak();
+                // Leak the webp
+                // Since there isn't a leak method, we manually leak it
+                let preview = unsafe {
+                    let ptr = transmute::<_, &'static [u8]>(preview.deref());
+                    forget(preview);
+                    ptr
+                };
 
-            // Leak the webp
-            // Since there isn't a leak method, we manually leak it
-            let preview = unsafe {
-                let ptr = transmute::<_, &'static [u8]>(preview.deref());
-                forget(preview);
-                ptr
-            };
+                let fname = path.file_name().unwrap();
 
-            let fname = path.file_name().unwrap();
+                let image_name = fname
+                    .to_str()
+                    .expect(&format!("filename to be valid: {fname:?}"))
+                    .to_owned();
 
-            let image_name = fname
-                .to_str()
-                .expect(&format!("filename to be valid: {fname:?}"))
-                .to_owned();
-
-            let _ = ready_sender.send((
-                image_name,
-                preview,
-                full
-            ));
+                let _ = image_sender.send((i, image_name, preview, full));
+            });
         });
+
+    let mut home_page_lines = Vec::with_capacity(entry_count);
+
+    for _ in 0..entry_count {
+        home_page_lines.push(MaybeUninit::uninit());
     }
 
-    drop(ready_sender);
-
     let mut router = Router::new();
-    let mut home_page_body = String::with_capacity(0);
     // In-memory zip of all images
     let mut all_zip = ZipWriter::new(Cursor::new(Vec::new()));
 
-    while let Ok((image_name, preview_image, full_image)) = ready_receiver.recv() {
+    while let Ok((i, image_name, preview_image, full_image)) = image_receiver.recv() {
         let preview_name = format!("/preview_{image_name}");
 
         // Register handlers for preview and full images
@@ -146,7 +131,7 @@ async fn main() {
                     resp.headers_mut()
                         .insert("Content-Type", HeaderValue::from_static("image/jpeg"));
                     resp
-                })
+                }),
             )
             .route(
                 &preview_name,
@@ -155,13 +140,16 @@ async fn main() {
                     resp.headers_mut()
                         .insert("Content-Type", HeaderValue::from_static("image/webp"));
                     resp
-                })
+                }),
             );
 
         // Add image to home page
-        home_page_body.push_str(
-            &format!("<a href=\"{image_name}\"><img src=\"{preview_name}\" style=\"width:900px;height:600px;\"></a><br>")
-        );
+        home_page_lines
+            .get_mut(i)
+            .unwrap()
+            .write(
+                format!("<a href=\"{image_name}\"><img src=\"{preview_name}\" style=\"width:900px;height:600px;\"></a><br>")
+            );
 
         // Zip
         all_zip
@@ -170,6 +158,11 @@ async fn main() {
 
         all_zip.write_all(full_image).expect("image to zip");
     }
+
+    let home_page_body = home_page_lines
+        .into_iter()
+        .map(|x| unsafe { MaybeUninit::assume_init(x) })
+        .collect::<String>();
 
     // Finalize the zip and leak that data too
     let all_zip: &[u8] = all_zip
@@ -182,40 +175,40 @@ async fn main() {
 
     let home_page_doc = format!(
         "<html>
-    <head>
-        <link href =\"https://fonts.googleapis.com\" rel=\"preconnect\">
-        <link href=\"https://fonts.googleapis.com/css?family=Open+Sans\" rel=\"stylesheet\">
-        <style>
-            * {{
-                font-family: 'Open Sans';
-                color: white;
-                text-align: center;
-            }}
-            body {{
-                background-color: #0c0c0c;
-            }}
-            
-            p {{
-                line-height: 35px;
-                max-width: 800px;
-                margin: auto;
-            }}
-            
-            h1 {{
-                font-size: 60px;
-            }}
-            
-            h2 {{
-                font-size: 40px;
-            }}
+<head>
+    <link href =\"https://fonts.googleapis.com\" rel=\"preconnect\">
+    <link href=\"https://fonts.googleapis.com/css?family=Open+Sans\" rel=\"stylesheet\">
+    <style>
+        * {{
+            font-family: 'Open Sans';
+            color: white;
+            text-align: center;
+        }}
+        body {{
+            background-color: #0c0c0c;
+        }}
+        
+        p {{
+            line-height: 35px;
+            max-width: 800px;
+            margin: auto;
+        }}
+        
+        h1 {{
+            font-size: 60px;
+        }}
+        
+        h2 {{
+            font-size: 40px;
+        }}
 
-            a {{
-                text-decoration: none;
-            }}
-        </style>
-    </head>
-    <body>
-            <a href=\"images.zip\" download><h2>Download All</h2></a>
+        a {{
+            text-decoration: none;
+        }}
+    </style>
+</head>
+<body>
+    <a href=\"images.zip\" download><h2>Download All</h2></a>
 {home_page_body}
     </body>
 </html>"
@@ -248,6 +241,7 @@ async fn main() {
 
     // Allow ctrl-c to be gracefully handled
     let fut = async {
+        println!("Deployed to all interfaces!");
         if let Err(e) = tokio::signal::ctrl_c().await {
             error!(target: "console_server", "Faced the following error while listening for ctrl_c: {e:?}");
             return;
@@ -255,10 +249,21 @@ async fn main() {
         println!("Ending...");
     };
 
-    println!("Deployed to all interfaces!");
+    // Get port from command line or default to 80
+    let port = match std::env::args().last().unwrap().parse() {
+        Ok(n) => {
+            println!("Deploying to {n}");
+            n
+        }
+        Err(_) => {
+            println!("Defaulting to 80");
+            80
+        }
+    };
+
     Server::bind(&std::net::SocketAddr::V4(SocketAddrV4::new(
         Ipv4Addr::new(0, 0, 0, 0),
-        80,
+        port,
     )))
     .serve(router.into_make_service())
     .with_graceful_shutdown(fut)
